afbf8c030476922d740d68e6935805f9c3929656
Tony O'Donnell <tonyod@gmail.com>
2024-05-18 18:25:41 +0100

    initial custom changes logos new users pushed to custom-changes branch

--- eventcatalog.config.js
+++ eventcatalog.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'EventCatalog',
+   title: 'EI Event Catalogue',
   tagline: 'Discover, Explore and Document your Event Driven Architectures',
-  organizationName: 'Your Company',
-  projectName: 'Event Catalog',
-  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  organizationName: 'easyJet',
+  projectName: 'EI Event Catalogue',
+  editUrl: 'https://github.com/tonyodonnell/eventcatalog/edit/master',
   trailingSlash: true,
   primaryCTA: {
     label: 'Explore Events',
@@ -16,7 +16,7 @@ module.exports = {
   logo: {
     alt: 'EventCatalog Logo',
     // found in the public dir
-    src: 'logo.svg',
+    src: 'easyjet.png',
   },
   headerLinks: [
     { label: 'Events', href: '/events'},
@@ -31,9 +31,34 @@ module.exports = {
     { label: 'Services', href: '/services' },
     { label: 'Visualiser', href: '/visualiser' },
     { label: '3D Node Graph', href: '/overview' },
-    { label: 'GitHub', href: 'https://github.com/boyney123/eventcatalog-demo/edit/master' }
+    { label: 'GitHub', href: 'https://github.com/boyney123/eventcatalog-demo/edit/master' },
+    { label: 'Confluence', href: 'https://easyjet.atlassian.net/wiki/spaces/EA/pages/247529520/Event+Catalogue' },
   ],
   users: [
+    {
+      id: 'todonnell',
+      name: 'Tony O Donnell',
+      avatarUrl: 'https://randomuser.me/api/portraits/lego/1.jpg',
+      role: 'TA',
+    },
+    {
+      id: 'rshanmugam',
+      name: 'Ravi Shanmugam',
+      avatarUrl: 'https://randomuser.me/api/portraits/lego/3.jpg',
+      role: 'Developer',
+    },
+    {
+      id: 'nkelsall',
+      name: 'Nick Kelsall',
+      avatarUrl: 'https://randomuser.me/api/portraits/lego/2.jpg',
+      role: 'Developer',
+    },
+    {
+      id: 'dfoskett',
+      name: 'David Foskett',
+      avatarUrl: 'https://randomuser.me/api/portraits/lego/4.jpg',
+      role: 'Developer',
+    },
     {
       id: 'dboyne',
       name: 'David Boyne',
@@ -41,10 +66,10 @@ module.exports = {
       role: 'Developer',
     },
     {
-      id: 'mSmith',
-      name: 'Matthew Smith',
-      avatarUrl: 'https://randomuser.me/api/portraits/lego/3.jpg',
-      role: 'Developer',
+      id: 'sAllison',
+      name: 'Steve Allison',
+      avatarUrl: 'https://randomuser.me/api/portraits/lego/5.jpg',
+      role: 'EA',
     },
   ],
 }
